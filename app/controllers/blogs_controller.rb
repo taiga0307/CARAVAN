a6e2f0dedbbac9b05fd564aecbd37357c25ca37d
yoneyama-taiga <tai03.ga07@gmail.com>
2020-04-07 14:29:04 +0900

    [Finish4-6]

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,10 +16,17 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blog_path(blog.id) # この行を修正
+    redirect_to blog_path(blog.id)
   end
 
   def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
   end
 
   private
